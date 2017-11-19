67224aa9f0e1a83317ce87faad7339b3e5de6da2
Felix <felixletsplay@outlook.de>
2017-11-19 19:50:50 +0100

    Changed Command Listner

--- de/HannesGames/HGBot/Main.java
+++ de/HannesGames/HGBot/Main.java
@@ -4,11 +4,16 @@ import com.cavariux.twitchirc.Chat.Channel;
 import de.HannesGames.HGBot.util.data.Config;
 
 public class Main {
+    private static HGBot bot;
     public static void main(String[] args) {
-        HGBot bot = new HGBot();
+        bot = new HGBot();
         bot.connect();
         Channel channel = bot.joinChannel("#" + Config.getTwitchAccount());
-        bot.sendMessage("test", channel);
+        bot.sendMessage("HannesGames Bot started!", channel);
         bot.start();
+    }
+
+    public static HGBot getBot() {
+        return bot;
     }
 }
